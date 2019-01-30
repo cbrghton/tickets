099920edca39e6ca5ac7dd59fbf971f3e9c58ef3
Carlos Brighton <cbrghton@gmail.com>
2019-01-29 18:24:01 -0600

    Editar Incidencia

---
+++ resources/views/tickets/editar.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('title', 'Editar Ticket')
+
+@section('content')
+    <div class="panel panel-flat">
+        <div class="panel-heading">
+            <h6 class="panel-title">Editar Ticket</h6>
+        </div>
+
+        <div class="panel-body">
+            <div class="form-group">
+                <h6>Incidencia</h6>
+
+                <textarea cols="100"></textarea>
+            </div>
+        </div>
+    </div>
+@endsection
