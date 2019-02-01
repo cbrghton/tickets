37b9f619744d87ff7c19c017ff485bb0119fe83f
victorpalma <vicom011001@hotmail.com>
2019-02-01 11:02:57 -0600

    Resetear password vista

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,11 +1,11 @@
 <div class="modal fade modal-slide-in-right" aria-hidden="true" role="dialog" tobindex="-1" id="modal-password-2">
     <div class="modal-dialog">
         <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">×</button>
+                <h5 class="modal-title">Resetear contraseña</h5>
+            </div>
             <div class="modal-body">
-
-                <div class="panel-heading">
-                    <h5 class="modal-title">Resetear contraseña</h5>
-                </div>
                 <div class="form-group row">
                     <label class="col-md-4 col-form-label text-md-right" for="rfc">RFC:</label>
                     <div class="col-md-5">
@@ -34,10 +34,10 @@
                         <input id="confirmar_password" type="password" class="form-control" name="confirmae_password" required>
                     </div>
                 </div>
-                <div class="form-group" align="right">
-                    <button type="button" class="btn btn-link" data-dismiss="modal">Cerrar</button>
-                    <button type="submit" class="btn btn-primary">Cambio de contraseña</button>
-                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-link" data-dismiss="modal">Cerrar</button>
+                <button type="submit" class="btn btn-primary">Cambio de contraseña</button>
             </div>
         </div>
     </div>
