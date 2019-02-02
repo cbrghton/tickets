42e39703452724e3dac20c9c40f6a2abe5dba2ca
Carlos Brighton <cbrghton@gmail.com>
2019-02-01 21:08:53 -0600

    Estilos

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -1,4 +1,11 @@
 @extends('layouts.app')
+
+@push('assets')
+    <script type="text/javascript" src="{{ asset('assets/js/plugins/forms/selects/select2.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/plugins/forms/styling/uniform.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/pages/custom/editar_ticket.js') }}"></script>
+@endpush
+
 @section('title','Crear Usuario')
 @section('content')
 
@@ -17,74 +24,71 @@
     </div>
     @endif
     <div class="panel-body">
-        {!!Form::open(array('url'=>'auth/index','method'=>'POST','autocomplete'=>'off'))!!}
-        {{Form::token()}}
-
-             <div class="col-lg-10 col-sm-10 col-md-10 col-xs-10">
+        <form>
+            <div class="col-lg-10 col-sm-10 col-md-10 col-xs-10">
                 <div class="form-group">
                     <label for="nombre"> Nombre  </label>
                     <input type="text" name="nombre"  class="form-control" placeholder="Nombre...." onkeyup="mayus(this)">
                 </div>
-             </div>
+            </div>
 
-              <div class="col-lg-10 col-sm-10 col-md-10 col-xs-10">
-                 <div class="form-group">
+            <div class="col-lg-10 col-sm-10 col-md-10 col-xs-10">
+                <div class="form-group">
                     <label for="primer_apellido"> Primer Apellido  </label>
                     <input type="text" name="primer_apellido"  class="form-control" placeholder="Primer Apellido...." onkeyup="mayus(this)">
                 </div>
-             </div>
+            </div>
 
-             <div class="col-lg-10 col-sm-10 col-md-10 col-xs-10">
-                 <div class="form-group">
-                      <label for="segundo_apellido"> Segundo Apellido  </label>
-                      <input type="text" name="segundo_apellido"  class="form-control" placeholder="Segundo Apellido...." onkeyup="mayus(this)">
-                 </div>
-             </div>
+            <div class="col-lg-10 col-sm-10 col-md-10 col-xs-10">
+                <div class="form-group">
+                    <label for="segundo_apellido"> Segundo Apellido  </label>
+                    <input type="text" name="segundo_apellido"  class="form-control" placeholder="Segundo Apellido...." onkeyup="mayus(this)">
+                </div>
+            </div>
 
-             <div class="col-lg-10 col-sm-10 col-md-10 col-xs-10">
-               <div class="form-group">
-                <label for="rfc"> RFC  </label>
-                <input type="text" name="rfc"  class="form-control" placeholder="RFC...." onkeyup="mayus(this)">
-                 </div>
-              </div>
+            <div class="col-lg-10 col-sm-10 col-md-10 col-xs-10">
+                <div class="form-group">
+                    <label for="rfc"> RFC  </label>
+                    <input type="text" name="rfc"  class="form-control" placeholder="RFC...." onkeyup="mayus(this)">
+                </div>
+            </div>
 
-        <div class="col-lg-10 col-sm-10 col-md-10 col-xs-10">
-            <div class="form-group">
-                <label for="rfc"> Modulo </label>
-                    <select class="form-control" name="modulo">
+            <div class="col-lg-10 col-sm-10 col-md-10 col-xs-10">
+                <div class="form-group">
+                    <label for="rfc"> Modulo </label>
+                    <select class="select" name="modulo">
                         <option value="modulo">Insurgentes</option>
                         <option value="modulo">Popotla</option>
                         <option value="modulo">Pilares</option>
                     </select>
+                </div>
             </div>
-        </div>
 
-        <div class="col-lg-10 col-sm-10 col-md-10 col-xs-10">
-            <div class="form-group">
-                <label for="password"> Password  </label>
-                <input type="password" name="password"  class="form-control" placeholder="Password....">
+            <div class="col-lg-10 col-sm-10 col-md-10 col-xs-10">
+                <div class="form-group">
+                    <label for="password"> Password  </label>
+                    <input type="password" name="password"  class="form-control" placeholder="Password....">
+                </div>
             </div>
-        </div>
 
-        <div class="col-lg-10 col-sm-10 col-md-10 col-xs-10">
-            <div class="form-group">
-                <label for="password"> Confirmar Password  </label>
-                <input type="password" name="password_confirmed"  class="form-control" placeholder="Confirmar Password....">
+            <div class="col-lg-10 col-sm-10 col-md-10 col-xs-10">
+                <div class="form-group">
+                    <label for="password"> Confirmar Password  </label>
+                    <input type="password" name="password_confirmed"  class="form-control" placeholder="Confirmar Password....">
+                </div>
             </div>
-        </div>
 
-        <div class="col-lg-10 col-sm-10 col-md-10 col-xs-10">
-            <div class="form-group">
-                <label for="rfc"> Rol </label>
-                <select class="form-control" name="rol">
-                    <option value="rol">Administrador</option>
-                    <option value="rol">Responsable</option>
-                    <option value="rol">Usuario</option>
-                </select>
+            <div class="col-lg-10 col-sm-10 col-md-10 col-xs-10">
+                <div class="form-group">
+                    <label for="rfc"> Rol </label>
+                    <select class="select" name="rol">
+                        <option value="rol">Administrador</option>
+                        <option value="rol">Responsable</option>
+                        <option value="rol">Usuario</option>
+                    </select>
+                </div>
             </div>
-        </div>
-
-        {!!Form::close()!!}
+        </form>
     </div>
     <script>
     function mayus(e) {
