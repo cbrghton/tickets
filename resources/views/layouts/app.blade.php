df920253a39f9ed2753fc8942810387ef9f398dc
Carlos Brighton <cbrghton@gmail.com>
2019-02-01 20:58:28 -0600

    Estilos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -177,13 +177,18 @@
                     <div class="category-content">
                         <div class="row">
                             <div class="col-xs-6">
-                                <button class="btn bg-teal-400 btn-block btn-float btn-float-lg text-size-small"
-                                        type="button"><i class="icon-home"></i> <span>Crear Ticket</span></button>
+                                <a href="{{ route('create_ticket') }}">
+                                    <button class="btn bg-teal-400 btn-block btn-float btn-float-lg text-size-small"
+                                            type="button"><i class="icon-home"></i> <span>Crear Ticket</span></button>
+                                </a>
                             </div>
 
                             <div class="col-xs-6">
-                                <a href="{{ route('see_tickets') }}"><button class="btn bg-warning-400 btn-block btn-float btn-float-lg text-size-small"
-                                        type="button"><i class="icon-stats-bars"></i><span>Ver Tickets</span></button></a>
+                                <a href="{{ route('see_tickets') }}">
+                                    <button class="btn bg-warning-400 btn-block btn-float btn-float-lg text-size-small"
+                                            type="button"><i class="icon-stats-bars"></i><span>Ver Tickets</span>
+                                    </button>
+                                </a>
                             </div>
                         </div>
                     </div>
--- resources/views/tickets/create.blade.php
+++ resources/views/tickets/create.blade.php
@@ -1,10 +1,16 @@
 @extends('layouts.app')
+
+@push('assets')
+    <script type="text/javascript" src="{{ asset('assets/js/plugins/forms/selects/select2.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/plugins/forms/styling/uniform.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/pages/custom/editar_ticket.js') }}"></script>
+@endpush
 @section('title','Crear Ticket')
 @section('content')
 
     <div class="panel panel-flat">
         <div class="panel-heading">
-            <h3>NuevO Ticket </h3>
+            <h3>Nuevo Ticket </h3>
             @if(count($errors)>0)
                 <div class="alert alert-danger">
                     <ul>
@@ -15,46 +21,44 @@
                 </div>
         </div>
     </div>
-            @endif
-        <div class="panel-body">
-    {!!Form::open(array('url'=>'tickets/index','method'=>'POST','autocomplete'=>'off','files'=>'true'))!!}
-    {{Form::token()}}
-
-         <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
-             <div class="form-group purple-border">
-                <label for="Incidencia">Incidencia</label>
-                <textarea class="form-control" name="incidencia" id="incidencia" rows="10" cols="20" onkeyup="mayus(this)"></textarea>
+    @endif
+    <div class="panel-body">
+        <form>
+            <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+                <div class="form-group purple-border">
+                    <label for="Incidencia">Incidencia</label>
+                    <textarea class="form-control" name="incidencia" id="incidencia" rows="10" cols="20"
+                              onkeyup="mayus(this)"></textarea>
+                </div>
             </div>
-         </div>
 
-        <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
-            <div class="form-group">
-            <label>Sistema</label>
-            <select name="cat_sistema_id_sistema" id="cat_sistema_id_sistema" class="form-control" >
+            <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+                <div class="form-group">
+                    <label>Sistema</label>
+                    <select name="cat_sistema_id_sistema" id="cat_sistema_id_sistema" class="select">
+                        <option></option>
+                        <option value="Alfred">Alfred</option>
+                        <option value="Revista Taxi">Alfred</option>
+                        <option value="Evee">Alfred</option>
 
-                <option value="Alfred">Alfred</option>
-                <option value="Revista Taxi">Alfred</option>
-                <option value="Evee">Alfred</option>
-
-            </select>
+                    </select>
+                </div>
             </div>
-        </div>
 
-        <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
-            <div class="form-group">
-                <label for="descripcion">  Imagen  </label>
-                <input type="file" name="imagen" class="form-control" placeholder="Descripcion.." >
+            <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+                <div class="form-group">
+                    <label class="display-block">Agrega más imagenes:</label>
+                    <input type="file" class="file-styled">
+                </div>
             </div>
-        </div>
 
-        <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
-            <div class="form-group">
-                <button class="btn btn-primary" type="submit">Guardar</button>
-                <button class="btn btn-danger" type="reset">Cancelar</button>
+            <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+                <div class="form-group">
+                    <button class="btn btn-primary" type="submit">Guardar</button>
+                    <button class="btn btn-danger" type="reset">Cancelar</button>
+                </div>
             </div>
-        </div>
-
-        {!!Form::close()!!}
+        </form>
     </div>
     <script>
         function mayus(e) {
