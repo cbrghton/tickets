20fdca86f66f32213a0793922721696a25692133
Carlos Brighton <cbrghton@gmail.com>
2019-02-19 20:38:49 -0600

    Push

--- database/factories/CatModuloFactory.php
+++ database/factories/CatModuloFactory.php
@@ -4,8 +4,6 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\CatModulo::class, function (Faker $faker) {
     return [
-        'clave_modulo' => $faker->numberBetween(0, 10),
         'modulo' => $faker->streetName,
-        'estatus' => 'ALTA'
     ];
 });
--- database/factories/CatSistemaFactory.php
+++ database/factories/CatSistemaFactory.php
@@ -4,8 +4,6 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\CatSistema::class, function (Faker $faker) {
     return [
-        'clave_sistema' => $faker->unique()->numberBetween(0, 10),
         'sistema' => $faker->name,
-        'estatus' => 'ALTA'
     ];
 });
